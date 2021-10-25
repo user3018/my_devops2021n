86d8938575aeb02070866adbab96f86d3b1f016b
Alexander Kuntashov <kuntashov@gmail.com>
2021-10-26 01:27:46 +0400

    Initial commit

---
+++ VanessaAdd.cmd
@@ -0,0 +1,3 @@
+@chcp 65001
+
+call runner vanessa --settings .\tools\JSON\VanessaAddRunner.json
---
+++ prepare.cmd
@@ -0,0 +1,14 @@
+@chcp 65001
+
+@rem Сборка основной разработческой ИБ. по умолчанию в каталоге build/ib
+call vrunner init-dev --src src/cf %*
+
+@rem собрать внешние обработчики и отчеты в каталоге build
+@rem call vrunner compileepf src/epf/МояВнешняяОбработка build %*
+@rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
+
+@rem собрать расширения конфигурации внутри ИБ
+@rem call vrunner compileext src/cfe/МоеРасширение МоеРасширение %*
+
+@rem Обновление в режиме Предприятия
+call vrunner run --command "ЗапуститьОбновлениеИнформационнойБазы;ЗавершитьРаботуСистемы;" --execute $runnerRoot\epf\ЗакрытьПредприятие.epf %*
