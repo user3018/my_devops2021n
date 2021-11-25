badb7ec5ad08b981a08974182e039fb75b786fa7
Alexander Kuntashov <kuntashov@gmail.com>
2021-11-25 09:45:26 +0400

    Исправлено некорректное имя переменной окружения

--- tools/acc-export/acc.bat
+++ tools/acc-export/acc.bat
@@ -1,4 +1,4 @@
 @set RUNNER_ibconnection="/FD:/Базы данных/АПК"
-@set RUNNER_db-user="Автор"
+@set RUNNER_dbuser="Автор"
 
 @call runner run --command "acc.propertiesPaths=D:\repo\kurs_devops_1\tools\acc-export\acc.properties;" --execute "D:\repo\kurs_devops_1\tools\acc-export\acc-export.epf" --ordinaryapp 1
