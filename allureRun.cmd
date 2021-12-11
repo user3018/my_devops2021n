4f7b0c6b609fcc2364e1838ef44a7d87fde956cc
User 3018 <user3018@yandex.ru>
2021-12-12 00:19:18 +0300

    Модульные тесты

--- allureRun.cmd
+++ allureRun.cmd
@@ -1,4 +1,4 @@
 @chcp 65001
 
-call allure generate --clean .\out\allure .\out\smoke\allure .\out\syntax-check\allure -o .\out\allure\allure-report && allure open .\out\allure\allure-report
+call allure generate --clean .\out\allure .\out\smoke\allure .\out\syntax-check\allure .\build\tests\allure -o .\out\allure\allure-report && allure open .\out\allure\allure-report
 # Для PowerShell: allure generate --clean .\out\allure .\out\smoke\allure .\out\syntax-check\allure -o .\out\allure\allure-report; allure open .\out\allure\allure-report
